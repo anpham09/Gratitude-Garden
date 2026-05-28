f53736e5010668ef4990ba7e0337673bb5fb6a26
An Pham <anpham.0992@gmail.com>
2026-05-28 18:33:15 -0400

    skeleton done

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const gratitudeInput = document.getElementById("gratitudeInput");
+const plantButton = document.getElementById("plantButton");
+const garden = document.getElementById("garden");
+
+plantButton.addEventListener("click", function () {
+    alert("Your seed has been planted!")
+});
